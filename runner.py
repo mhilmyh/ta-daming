a6b389f64ee7ada984cf5cfb1b2b70b62c0ef5a5
mhilmyh <hilmy_21@apps.ipb.ac.id>
2020-05-30 21:10:00 +0700

    simple SIR model

---
+++ runner.py
@@ -0,0 +1,17 @@
+from model import SIR
+import constant as c
+import numpy as np
+
+if __name__ == '__main__':
+    """
+    Asumsi waktu pandemi yang berlangsung
+    yaitu 160 hari 
+    """
+    days = np.linspace(0, 160, 160)
+
+    """
+    Model SIR dibuat dengan memberikan
+    parameter yang dibutuhkan
+    """
+    sir = SIR(c.TOTAL_POPULATION, I0=c.I0, b=c.BETA, g=c.GAMMA, t=days)
+    sir.integrate()
